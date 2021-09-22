de14ae764dc6734229e145acbcc56e71bde3bb01
Luigi <luigi.gottardello.fonseca@gmail.com>
2021-09-22 10:23:42 -0300

    Finalizando o Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   background: var(--blue);
-
-
-
 `;
 
 export const Content = styled.div`
@@ -25,5 +22,10 @@ export const Content = styled.div`
     border-radius: 0.25rem;
     height: 3rem;
 
+    transition: filter 0.2s;
+    
+    &:hover {
+      filter: brightness(0.9);
+    }
   }
 `;
